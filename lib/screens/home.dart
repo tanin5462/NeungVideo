a59ee6477fe10ae5132e33ad32fd12de8fff1126
tanin <ber_zebub@hotmail.com>
2019-08-12 10:40:07 +0700

    Design UI Home Success

---
+++ lib/screens/home.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // Explicit (Variable)
+  double mySizeLogo = 120.0;
+  Color myColor = Color.fromARGB(0xFF, 0x00, 0x3c, 0x8f);
+  // Method
+
+  Widget showLogo() {
+    return Container(
+      width: mySizeLogo,
+      height: mySizeLogo,
+      child: Image.asset('images/logo.png'),
+    );
+  }
+
+  Widget showAppName() {
+    return Text(
+      "BE12ZEBUB TEST2",
+      style: TextStyle(
+        fontSize: 30.0,
+        color: myColor,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic,
+        fontFamily: 'Prompt',
+      ),
+    );
+  }
+
+  Widget showButton() {
+    return Container(
+      width: 250.0,
+      child: Row(
+        children: <Widget>[
+          signInButton(),
+          mySizeBox(),
+          signUpButton(),
+          mySizeBox(),
+        ],
+      ),
+    );
+  }
+
+  Widget signInButton() {
+    return Expanded(
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        color: myColor,
+        child: Text(
+          'signIn',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget signUpButton() {
+    return Expanded(
+      child: OutlineButton(
+        borderSide: BorderSide(color: myColor),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Text(
+          'signUp',
+          style: TextStyle(color: myColor),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget mySizeBox() {
+    return SizedBox(
+      width: 12.0,
+      height: 24.0,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              radius: 0.9,
+              colors: [
+                Colors.white,
+                Colors.blueGrey[600],
+              ],
+            ),
+          ),
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              showLogo(),
+              showAppName(),
+              mySizeBox(),
+              showButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
